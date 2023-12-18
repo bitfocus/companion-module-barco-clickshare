f98ecd6f0bd61ad5ed33fbdba8d6808c514fc448
Julian Waller <git@julusian.co.uk>
2023-12-18 22:12:37 +0000

    chore: update prettier config

--- src/barco-api/barco.ts
+++ src/barco-api/barco.ts
@@ -1,79 +1,76 @@
-import { Agent } from 'https';
-import fetch, { HeadersInit, Response } from 'node-fetch';
+import { Agent } from 'https'
+import fetch, { HeadersInit, Response } from 'node-fetch'
 
 class HTTPResponseError extends Error {
-  response: any;
+	response: any
 	constructor(response: Response) {
-		super(`HTTP Error Response: ${response.status} ${response.statusText}`);
-		this.response = response;
+		super(`HTTP Error Response: ${response.status} ${response.statusText}`)
+		this.response = response
 	}
 }
 
 interface BarcoSystemStatusResponse {
-  currentUptime: number;
-  errorCode: string;
-  errorMessage: string;
-  firstUsed: string;
-  inUse: boolean;
-  sharing: boolean;
-  totalUptime: number;
+	currentUptime: number
+	errorCode: string
+	errorMessage: string
+	firstUsed: string
+	inUse: boolean
+	sharing: boolean
+	totalUptime: number
 }
 
 export interface InUseStatus {
-  inUse: boolean,
-  sharing: boolean
+	inUse: boolean
+	sharing: boolean
 }
 export default class BarcoApi {
-  private _host: string;
-  private _port: number;
-  private _credentials: string;
-  private _defaultHeaders: HeadersInit;
-  private _agent: Agent;
-  private _apiVersion: string;
+	private _host: string
+	private _port: number
+	private _credentials: string
+	private _defaultHeaders: HeadersInit
+	private _agent: Agent
+	private _apiVersion: string
 
-  constructor(host: string, port: number, user: string, pass: string) {
-    this._host = host;
-    this._port = port;
-    this._apiVersion = 'v2';
-    this._credentials = Buffer.from(`${user}:${pass}`).toString('base64');
-    this._defaultHeaders = {
-      Accept: 'application/json',
-      Authorization: `Basic ${this._credentials}`
-    };
-    this._agent = new Agent({  
-      rejectUnauthorized: false
-    });
-  }
+	constructor(host: string, port: number, user: string, pass: string) {
+		this._host = host
+		this._port = port
+		this._apiVersion = 'v2'
+		this._credentials = Buffer.from(`${user}:${pass}`).toString('base64')
+		this._defaultHeaders = {
+			Accept: 'application/json',
+			Authorization: `Basic ${this._credentials}`,
+		}
+		this._agent = new Agent({
+			rejectUnauthorized: false,
+		})
+	}
 
-  private checkStatus (response: Response) {
-    if (response.ok) {
-      // response.status >= 200 && response.status < 300
-      return response;
-    } else {
-      throw new HTTPResponseError(response);
-    }
-  }
+	private checkStatus(response: Response) {
+		if (response.ok) {
+			// response.status >= 200 && response.status < 300
+			return response
+		} else {
+			throw new HTTPResponseError(response)
+		}
+	}
 
-  //@ts-ignore
-  private authenticate(headers: Headers) {
-    headers.append('Authorization', `Basic ${this._credentials}`);
-  }
+	//@ts-ignore
+	private authenticate(headers: Headers) {
+		headers.append('Authorization', `Basic ${this._credentials}`)
+	}
 
-  public async isInUse(): Promise<InUseStatus> {
-    const url = `https://${this._host}:${this._port}/${this._apiVersion}/configuration/system/status`;
-    const response = await fetch(
-      url,
-      {
-        method: 'get',
-        headers: this._defaultHeaders,
-        agent: this._agent
-      });
-    this.checkStatus(response);
-    const data = await response.json() as BarcoSystemStatusResponse;
-    return {
-      inUse: data.inUse,
-      sharing: data.sharing
-    };
-  }
-} 
-
+	public async isInUse(): Promise<InUseStatus> {
+		const url = `https://${this._host}:${this._port}/${this._apiVersion}/configuration/system/status`
+		const response = await fetch(url, {
+			method: 'get',
+			headers: this._defaultHeaders,
+			agent: this._agent,
+		})
+		this.checkStatus(response)
+		const data = (await response.json()) as BarcoSystemStatusResponse
+		return {
+			inUse: data.inUse,
+			sharing: data.sharing,
+		}
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,250 +1,247 @@
-import { CompanionFeedbackEvent, SomeCompanionConfigField } from "../../../instance_skel_types";
-import BarcoApi from './barco-api/barco';
-import InstanceSkel from '../../../instance_skel';
-import sleep from './sleep';
+import { CompanionFeedbackEvent, SomeCompanionConfigField } from '../../../instance_skel_types'
+import BarcoApi from './barco-api/barco'
+import InstanceSkel from '../../../instance_skel'
+import sleep from './sleep'
 
 interface BarcoClickShareConfig {
-  host: string;
-  port: number;
-  user: string;
-  password: string;
+	host: string
+	port: number
+	user: string
+	password: string
 }
 
 ///
 class instance extends InstanceSkel<BarcoClickShareConfig> {
-  private _api: BarcoApi | null = null;
+	private _api: BarcoApi | null = null
 
 	constructor(system: any, id: any, config: any) {
-		super(system,id,config);
-    this.setupFeedback();
+		super(system, id, config)
+		this.setupFeedback()
 	}
 
-  public isInUse?: boolean = undefined;
-  public isSharing?: boolean = undefined;
-  private _subscriptions: number = 0;
-  
-  private get subscriptions(): number {
-    return this._subscriptions;
-  }
-  
-  private set subscriptions(value: number) {
-    this._subscriptions = value;
-    this.shouldBePolling = value > 0;
-    if (this.shouldBePolling && !this.isPolling) {
-      this.pollInUseStatus();
-      // it stops by itself
-    }
-  }
+	public isInUse?: boolean = undefined
+	public isSharing?: boolean = undefined
+	private _subscriptions: number = 0
 
-  private shouldBePolling: boolean = false;
-  private isPolling: boolean = false;
+	private get subscriptions(): number {
+		return this._subscriptions
+	}
 
-  /**
-   * Provide a simple return 
-   * of the necessary fields for the 
-   * instance configuration screen.
-   * @return {object[]}
-   */
-  config_fields(): SomeCompanionConfigField[] {
-    return [
-      {
-        type: 'textinput',
-        id: 'host',
-        label: 'Clickshare IP Address',
-        width: 6,
-        regex: this.REGEX_IP
-      },
-      {
-        type: 'number',
-        id: 'port',
-        label: 'Clickshare API Port',
-        width: 6,
-        min: 1,
-        max: 65536,
-        default: 4003
-      },
-      {
-        type: 'textinput',
-        id: 'user',
-        label: 'Clickshare API Username',
-        width: 6
-      },
-      {
-        type: 'textinput',
-        id: 'password',
-        label: 'Clickshare API Password',
-        width: 6
-      }
-    ];
-  }
+	private set subscriptions(value: number) {
+		this._subscriptions = value
+		this.shouldBePolling = value > 0
+		if (this.shouldBePolling && !this.isPolling) {
+			this.pollInUseStatus()
+			// it stops by itself
+		}
+	}
 
-  /**
-   * Main initialization function called once the module is 
-   * OK to start doing things. Principally, this is when 
-   * the module should establish a connection to the device.
-   * @return {void}
-   */
-   init(): void {
-     const config = this.config;
-     this._api = new BarcoApi(config.host, config.port, config.user, config.password);
-     this.subscribeFeedbacks();
-   }
+	private shouldBePolling: boolean = false
+	private isPolling: boolean = false
 
-  /**
-   * Clean up the instance before it is destroyed. 
-   * This is called both on shutdown and when an instance 
-   * is disabled or deleted. Destroy any timers and socket
-   * connections here.
-   * @return {void}
-   */
-  destroy(): void {
-    this._api = null;
-  }
+	/**
+	 * Provide a simple return
+	 * of the necessary fields for the
+	 * instance configuration screen.
+	 * @return {object[]}
+	 */
+	config_fields(): SomeCompanionConfigField[] {
+		return [
+			{
+				type: 'textinput',
+				id: 'host',
+				label: 'Clickshare IP Address',
+				width: 6,
+				regex: this.REGEX_IP,
+			},
+			{
+				type: 'number',
+				id: 'port',
+				label: 'Clickshare API Port',
+				width: 6,
+				min: 1,
+				max: 65536,
+				default: 4003,
+			},
+			{
+				type: 'textinput',
+				id: 'user',
+				label: 'Clickshare API Username',
+				width: 6,
+			},
+			{
+				type: 'textinput',
+				id: 'password',
+				label: 'Clickshare API Password',
+				width: 6,
+			},
+		]
+	}
 
-  /**
-   * Poll for in use status continuously until there are 
-   * no more subscriptions or until the module is destroyed
-   * @return {void}
-   */
-  private async pollInUseStatus() {
-    this.isPolling = true;
-    try {
-      // loop until we don't need to poll any more
-      while (this._api && this.shouldBePolling) {
-        // check the status via the api
-        try {
-          let status = await this._api.isInUse();
-          this.status(this.STATUS_OK);
-          if (this.isInUse !== status.inUse) {
-            // status changed
-            this.isInUse = status.inUse;
-            this.checkFeedbacks('available', 'inUse', 'idle');
-          }
-          if (this.isSharing !== status.sharing) {
-            // status changed
-            this.isSharing = status.sharing;
-            this.checkFeedbacks('available', 'sharing');
-          }
-        }
-        catch (e: any) {
-          this.status(this.STATUS_ERROR, e.message);
-        }
-        await sleep(750);
-      }
-    }
-    finally {
-      this.isPolling = false;
-    }
-  }
+	/**
+	 * Main initialization function called once the module is
+	 * OK to start doing things. Principally, this is when
+	 * the module should establish a connection to the device.
+	 * @return {void}
+	 */
+	init(): void {
+		const config = this.config
+		this._api = new BarcoApi(config.host, config.port, config.user, config.password)
+		this.subscribeFeedbacks()
+	}
 
-  /**
-   * When the instance configuration is saved by the user, 
-   * this update will fire with the new configuration
-   * @param {BarcoClickShareConfig} config
-   * @return {void}
-   */
-  updateConfig(config: BarcoClickShareConfig): void {
-    this.config = config;
-    this._api = new BarcoApi(config.host, config.port, config.user, config.password);
-  }
+	/**
+	 * Clean up the instance before it is destroyed.
+	 * This is called both on shutdown and when an instance
+	 * is disabled or deleted. Destroy any timers and socket
+	 * connections here.
+	 * @return {void}
+	 */
+	destroy(): void {
+		this._api = null
+	}
 
-  setupFeedback() {
-    this.setFeedbackDefinitions({
-      inUse: {
-        type: 'boolean', // Feedbacks can either a simple boolean, or can be an 'advanced' style change (until recently, all feedbacks were 'advanced')
-        label: 'In Use',
-        description: 'True if the app or a button is connected to the Clickshare',
-        style: {
-          // The default style change for a boolean feedback
-          // The user will be able to customise these values as well as the fields that will be changed
-          color: this.rgb(0, 0, 0),
-          bgcolor: this.rgb(255, 0, 0)
-        },
-        // options is how the user can choose the condition the feedback activates for
-        options: [],
-        callback: (_feedback: CompanionFeedbackEvent): boolean => {
-          // This callback will be called whenever companion wants to check if this feedback is 'active' and should affect the button style
-          return !!this.isInUse;    
-        },
-        subscribe: () => {
-          this.subscriptions++;
-        },
-        unsubscribe: () => {
-          this.subscriptions--;
-        }
-      },
-      sharing: {
-        type: 'boolean', // Feedbacks can either a simple boolean, or can be an 'advanced' style change (until recently, all feedbacks were 'advanced')
-        label: 'Sharing',
-        description: 'True if someone is streaming a desktop to the Clickshare',
-        style: {
-          // The default style change for a boolean feedback
-          // The user will be able to customise these values as well as the fields that will be changed
-          color: this.rgb(0, 0, 0),
-          bgcolor: this.rgb(255, 0, 0)
-        },
-        // options is how the user can choose the condition the feedback activates for
-        options: [],
-        callback: (_feedback: CompanionFeedbackEvent): boolean => {
-          // This callback will be called whenever companion wants to check if this feedback is 'active' and should affect the button style
-          return !!this.isSharing;    
-        },
-        subscribe: () => {
-          this.subscriptions++;
-        },
-        unsubscribe: () => {
-          this.subscriptions--;
-        }
-      },
-      idle: {
-        type: 'boolean', // Feedbacks can either a simple boolean, or can be an 'advanced' style change (until recently, all feedbacks were 'advanced')
-        label: 'Idle',
-        description: 'True if no one is connected to the Clickshare',
-        style: {
-          // The default style change for a boolean feedback
-          // The user will be able to customise these values as well as the fields that will be changed
-          color: this.rgb(0, 0, 0),
-          bgcolor: this.rgb(255, 0, 0)
-        },
-        // options is how the user can choose the condition the feedback activates for
-        options: [],
-        callback: (_feedback: CompanionFeedbackEvent) => {
-          // This callback will be called whenever companion wants to check if this feedback is 'active' and should affect the button style
-          return !this.isInUse;    
-        },
-        subscribe: () => {
-          this.subscriptions++;
-        },
-        unsubscribe: () => {
-          this.subscriptions--;
-        }
-      },
-      available: {
-        type: 'boolean', // Feedbacks can either a simple boolean, or can be an 'advanced' style change (until recently, all feedbacks were 'advanced')
-        label: 'Available',
-        description: 'True if some one is connected to the Clickshare but no one is streaming',
-        style: {
-          // The default style change for a boolean feedback
-          // The user will be able to customise these values as well as the fields that will be changed
-          color: this.rgb(0, 0, 0),
-          bgcolor: this.rgb(255, 0, 0)
-        },
-        // options is how the user can choose the condition the feedback activates for
-        options: [],
-        callback: (_feedback: CompanionFeedbackEvent) => {
-          // This callback will be called whenever companion wants to check if this feedback is 'active' and should affect the button style
-          return !!this.isInUse && !this.isSharing;    
-        },
-        subscribe: () => {
-          this.subscriptions++;
-        },
-        unsubscribe: () => {
-          this.subscriptions--;
-        }
-      }
-    });
-  }
+	/**
+	 * Poll for in use status continuously until there are
+	 * no more subscriptions or until the module is destroyed
+	 * @return {void}
+	 */
+	private async pollInUseStatus() {
+		this.isPolling = true
+		try {
+			// loop until we don't need to poll any more
+			while (this._api && this.shouldBePolling) {
+				// check the status via the api
+				try {
+					let status = await this._api.isInUse()
+					this.status(this.STATUS_OK)
+					if (this.isInUse !== status.inUse) {
+						// status changed
+						this.isInUse = status.inUse
+						this.checkFeedbacks('available', 'inUse', 'idle')
+					}
+					if (this.isSharing !== status.sharing) {
+						// status changed
+						this.isSharing = status.sharing
+						this.checkFeedbacks('available', 'sharing')
+					}
+				} catch (e: any) {
+					this.status(this.STATUS_ERROR, e.message)
+				}
+				await sleep(750)
+			}
+		} finally {
+			this.isPolling = false
+		}
+	}
 
+	/**
+	 * When the instance configuration is saved by the user,
+	 * this update will fire with the new configuration
+	 * @param {BarcoClickShareConfig} config
+	 * @return {void}
+	 */
+	updateConfig(config: BarcoClickShareConfig): void {
+		this.config = config
+		this._api = new BarcoApi(config.host, config.port, config.user, config.password)
+	}
+
+	setupFeedback() {
+		this.setFeedbackDefinitions({
+			inUse: {
+				type: 'boolean', // Feedbacks can either a simple boolean, or can be an 'advanced' style change (until recently, all feedbacks were 'advanced')
+				label: 'In Use',
+				description: 'True if the app or a button is connected to the Clickshare',
+				style: {
+					// The default style change for a boolean feedback
+					// The user will be able to customise these values as well as the fields that will be changed
+					color: this.rgb(0, 0, 0),
+					bgcolor: this.rgb(255, 0, 0),
+				},
+				// options is how the user can choose the condition the feedback activates for
+				options: [],
+				callback: (_feedback: CompanionFeedbackEvent): boolean => {
+					// This callback will be called whenever companion wants to check if this feedback is 'active' and should affect the button style
+					return !!this.isInUse
+				},
+				subscribe: () => {
+					this.subscriptions++
+				},
+				unsubscribe: () => {
+					this.subscriptions--
+				},
+			},
+			sharing: {
+				type: 'boolean', // Feedbacks can either a simple boolean, or can be an 'advanced' style change (until recently, all feedbacks were 'advanced')
+				label: 'Sharing',
+				description: 'True if someone is streaming a desktop to the Clickshare',
+				style: {
+					// The default style change for a boolean feedback
+					// The user will be able to customise these values as well as the fields that will be changed
+					color: this.rgb(0, 0, 0),
+					bgcolor: this.rgb(255, 0, 0),
+				},
+				// options is how the user can choose the condition the feedback activates for
+				options: [],
+				callback: (_feedback: CompanionFeedbackEvent): boolean => {
+					// This callback will be called whenever companion wants to check if this feedback is 'active' and should affect the button style
+					return !!this.isSharing
+				},
+				subscribe: () => {
+					this.subscriptions++
+				},
+				unsubscribe: () => {
+					this.subscriptions--
+				},
+			},
+			idle: {
+				type: 'boolean', // Feedbacks can either a simple boolean, or can be an 'advanced' style change (until recently, all feedbacks were 'advanced')
+				label: 'Idle',
+				description: 'True if no one is connected to the Clickshare',
+				style: {
+					// The default style change for a boolean feedback
+					// The user will be able to customise these values as well as the fields that will be changed
+					color: this.rgb(0, 0, 0),
+					bgcolor: this.rgb(255, 0, 0),
+				},
+				// options is how the user can choose the condition the feedback activates for
+				options: [],
+				callback: (_feedback: CompanionFeedbackEvent) => {
+					// This callback will be called whenever companion wants to check if this feedback is 'active' and should affect the button style
+					return !this.isInUse
+				},
+				subscribe: () => {
+					this.subscriptions++
+				},
+				unsubscribe: () => {
+					this.subscriptions--
+				},
+			},
+			available: {
+				type: 'boolean', // Feedbacks can either a simple boolean, or can be an 'advanced' style change (until recently, all feedbacks were 'advanced')
+				label: 'Available',
+				description: 'True if some one is connected to the Clickshare but no one is streaming',
+				style: {
+					// The default style change for a boolean feedback
+					// The user will be able to customise these values as well as the fields that will be changed
+					color: this.rgb(0, 0, 0),
+					bgcolor: this.rgb(255, 0, 0),
+				},
+				// options is how the user can choose the condition the feedback activates for
+				options: [],
+				callback: (_feedback: CompanionFeedbackEvent) => {
+					// This callback will be called whenever companion wants to check if this feedback is 'active' and should affect the button style
+					return !!this.isInUse && !this.isSharing
+				},
+				subscribe: () => {
+					this.subscriptions++
+				},
+				unsubscribe: () => {
+					this.subscriptions--
+				},
+			},
+		})
+	}
 }
 
-exports = module.exports = instance;
+exports = module.exports = instance
